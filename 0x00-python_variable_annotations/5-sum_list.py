ce9f5dd0eb8ae56e4629e21d7a009d2997dd8407
Damien Bernstein <111013117+DamienBernstein@users.noreply.github.com>
2023-07-06 18:35:48 +0200

    Create 5-sum_list.py

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Contains function sum_list which takes a list of floats and returns their sum."""
+
+
+def sum_list(input_list(n): float) -> float:
+    """Returns the sum of the input_list."""
+    return sum_list
