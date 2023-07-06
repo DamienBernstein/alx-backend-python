54ca4e97011ed3c1769e69a5db1031fd2a28d117
Damien Bernstein <111013117+DamienBernstein@users.noreply.github.com>
2023-07-06 18:37:26 +0200

    Update 5-sum_list.py

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -2,6 +2,9 @@
 """Contains function sum_list which takes a list of floats and returns their sum."""
 
 
-def sum_list(input_list(n): float) -> float:
+def sum_list(input_list: list[float]) -> float:
     """Returns the sum of the input_list."""
-    return sum_list
+    if input_list is None:
+        return 0
+    else:
+        return sum(input_list)
