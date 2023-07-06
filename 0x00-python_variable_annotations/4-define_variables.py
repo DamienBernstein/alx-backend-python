2e2034bb257755442c2787da055f961ee3dcbb14
Damien Bernstein <111013117+DamienBernstein@users.noreply.github.com>
2023-07-06 18:29:09 +0200

    Create 4-define_variables.py

---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Definition and annotation the following variables with the specified values."""
+
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: boolean = True
+school: str = "Holberton"
