9ee7e660e8125a06d5167cf2a228e0019f8a8913
Damien Bernstein <111013117+DamienBernstein@users.noreply.github.com>
2023-07-06 19:52:48 +0200

    Update 8-make_multiplier.py

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -4,14 +4,15 @@ from typing import Callable
 
 
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
-   """Multiplies a float by multiplier
+    """Multiplies a float by multiplier
     Args:
         multiplier (float): The multiplier
     Returns:
         A function that multiplies a float by multiplier
     """
-def multiplier_func(number: float) -> float:
-    """Multiplies a float by multiplier"""
-    return multiplier * number
 
-return multiplier_fun
+    def multiplier_func(number: float) -> float:
+        """Multiplies a float by multiplier"""
+        return multiplier * number
+
+    return multiplier_func
