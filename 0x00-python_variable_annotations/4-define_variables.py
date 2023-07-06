acba96ef7b78e4923ab796566aa0184ed7a6e1d7
Damien Bernstein <111013117+DamienBernstein@users.noreply.github.com>
2023-07-06 18:30:15 +0200

    Update 4-define_variables.py

--- 0x00-python_variable_annotations/4-define_variables.py
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
-"""Definition and annotation the following variables with the specified values."""
-
+"""Contains definition of variables with specified values and annotations."""
 
 a: int = 1
 pi: float = 3.14
-i_understand_annotations: boolean = True
+i_understand_annotations: bool = True
 school: str = "Holberton"
