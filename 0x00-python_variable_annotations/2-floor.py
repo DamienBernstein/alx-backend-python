1eed830322c283a3a952f2dc605168301cd322e4
Damien Bernstein <111013117+DamienBernstein@users.noreply.github.com>
2023-07-06 18:32:10 +0200

    Update 2-floor.py

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""Type-annotated function floor which takes a float n as argument and returns the floor of the float."""
+"""Contains a single function that calculates the floor of a number."""
 
 import math
 
