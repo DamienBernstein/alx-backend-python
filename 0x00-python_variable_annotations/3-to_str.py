54f5cd25e90729917225946f43a8beee8b4b5c05
Damien Bernstein <111013117+DamienBernstein@users.noreply.github.com>
2023-07-06 18:31:27 +0200

    Update 3-to_str.py

--- 0x00-python_variable_annotations/3-to_str.py
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""Type-annotated function to_str that takes a float n as argument and returns the string representation of the float."""
+"""Contains a function that returns a string representation of a float"""
 
 
 def to_str(n: float) -> str:
