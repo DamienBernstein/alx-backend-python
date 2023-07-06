26b13c4bc2d6de6a82bd6723b67059e0cb4a9aa8
Damien Bernstein <111013117+DamienBernstein@users.noreply.github.com>
2023-07-06 19:51:48 +0200

    Update 7-to_kv.py

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -3,6 +3,6 @@
 from typing import Union, Tuple
 
 
-def to_kv(k: str, v: Union[str, float]) -> Tuple[int, float]:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
      """Converts a Python variable to a KV pair."""
     return k, v **2
