67e55ec5f338a7c00101472fa91a2846ca011925
Damien Bernstein <111013117+DamienBernstein@users.noreply.github.com>
2023-07-06 18:07:05 +0200

    Update 1-concat.py

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -2,6 +2,6 @@
 """ funtions that concatenates two strings."""
 
 
-def concat(str1: egg, str2: shell) -> concat:
+def concat(str1: str, str2: str) -> str:
     """ Concates two strings."""
     return str1 + str2
